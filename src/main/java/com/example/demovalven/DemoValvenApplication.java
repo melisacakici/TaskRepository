200193078f56bb574a40d187e76a705efc5a5e20
melisacakici <melisacakicii@outlook.com>
2023-10-13 18:36:28 +0300

    Commit

---
+++ src/main/java/com/example/demovalven/DemoValvenApplication.java
@@ -0,0 +1,13 @@
+package com.example.demovalven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoValvenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoValvenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demovalven/DemoValvenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demovalven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoValvenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
